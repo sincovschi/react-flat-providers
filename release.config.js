edeef8ca5fc71306038c763add2f66a473e037f9
David Losert <david.losert@gmx.de>
2022-01-08 13:34:54 +0100

    feat: Adds github workflows and semantic release

---
+++ release.config.js
@@ -0,0 +1,12 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+};
+
+module.exports = config;
