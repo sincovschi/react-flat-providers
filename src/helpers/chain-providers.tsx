00ef26f57f997f2f53a4bf1a8c53c8c94f35c9d4
Eugene <0ggy.sin@gmail.com>
2022-02-14 16:49:09 +0200

    feat: add chain providers helper

---
+++ src/helpers/chain-providers.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FlatProviders, Providers } from '..';
+import { ProviderComponent, ProviderComponentProps } from '../types/provider';
+
+export function useChainProviders() {
+  const providers: Providers = [];
+
+  const ChainBuilder = {
+    /**
+     * Adds a new provider with its props to the stack.
+     * The chain must end into `make()`
+     */
+    add<T>(provider: ProviderComponent<T>, props?: ProviderComponentProps<T>) {
+      providers.push(props ? [provider, props] : provider);
+      return ChainBuilder;
+    },
+    /**
+     * Must be called once at the end of the Providers Chain.
+     */
+    make() {
+      const ChainedFlatProviders: React.FunctionComponent = ({ children }) => {
+        return <FlatProviders providers={providers}>{children}</FlatProviders>;
+      };
+
+      return ChainedFlatProviders;
+    },
+  };
+
+  return ChainBuilder;
+}
